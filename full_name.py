5daa729c06d497f094c26197f7571a6ee09ef55d
juicyj201 <220102473@mycput.ac.za>
2022-10-03 14:03:33 +0200

    Added changes to full_name files

--- full_name.py
+++ full_name.py
@@ -1,34 +1,54 @@
 import re
 from sys import argv
 import csv
-import os
+
+class Name:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
+
+
+    @property
+    def first(self):
+        return self._first
+
+
+    @first.setter
+    def first(self, first):
+        self._first = first if first != None else ValueError    
+
+
+    @property
+    def last(self):
+        return self._last
+
+
+    @last.setter
+    def last(self, last):
+        self._last = last if last != None else ValueError   
+
 
 def main():
     if argv[1] == "-n":
         first_last(str(input("What is your full name: ")))
-        open_file()
 
 
-
-def first_last(s: str):
-    if match := re.search(r'^([a-zA-Z])([ ])([a-zA-z])$', s):
-        name_dict = {
-            "First Name":match.group(1), 
-            "Last Name":match.group(3)
-        }
-
-        return save_to_file(name_dict)        
+def first_last(s):
+    if match := re.search(r'^(\w+)\s(\w+)$', s):
+        first = match.group(1)
+        last = match.group(2)
+        #print(match.group(1), match.group(2))
+        return save_to_file(first, last)        
 
 
-def save_to_file(**converted):
+def save_to_file(first, last):
     with open("full_name.csv", "a") as file:
-        writer = csv.DictWriter(file, fieldnames=["First Name", "Last Name"])
-        writer.writerow(converted)
-
-
-def open_file():
-    file = "./full_name.csv"
-    fp = os.open(file)
+        writer = csv.DictWriter(file, fieldnames=['First Name', 'Last Name'])
+        writer.writerow({'First Name': first, 'Last Name': last})
 
 
 if __name__ == "__main__":
